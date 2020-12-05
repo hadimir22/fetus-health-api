12d77cd978dd6cad866b414f96a831dde0522658
hadimir22 <hadimir22@gmail.com>
2020-12-05 10:44:03 +0530

    feat: api created to get predictions

--- app.py
+++ app.py
@@ -1,26 +1,23 @@
-from flask import Flask, request
-import joblib
+from flask import Flask, request, jsonify
 import pickle
+import numpy as np
 
 
 app = Flask(__name__)
 
-# model = joblib.load('./classifier_model.joblib')
-# print(model)
-#
-# @app.route('/predict', methods = ['POST'])
-
 model = pickle.load(open('model.pkl','rb'))
-print('model iss', model)
-@app.route('/')
-def hello():
+
+
+@app.route('/predict', methods = ['POST'])
+def predict():
 	try:
-		# data = request.form['data']
-		data = [[1,2,3,0,0,0,7,8,9,10,11]]
-		prediction = model.predict_proba(data)
+		data = request.get_json()
+		prediction = model.predict_proba([data['features']])
 		print(prediction)
-		return 'prediction'
-	except:
+		output = {'predictions': prediction.tolist()[0]}
+		return jsonify( output	)
+	except NameError:
+		print(NameError)
 		return 'something went wrong'
 
 
