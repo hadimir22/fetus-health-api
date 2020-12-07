4a64426412ff33f8f7cc12bd9fc3cfbd10fc0fe1
hadimir22 <hadimir22@gmail.com>
2020-12-07 21:52:45 +0530

    feat: CORS added

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
 import pickle
 
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 model = pickle.load(open('model.pkl','rb'))
 
 
 @app.route('/predict', methods = ['POST'])
+@cross_origin()
 def predict():
 	try:
 		data = request.get_json()
